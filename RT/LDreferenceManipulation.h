eeec5368bdb3cf0748a4d8c6e5e024ea5f2c3d7c
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:36:45 +1100

    NO CHANGE RECORD

--- RT/LDreferenceManipulation.h
+++ RT/LDreferenceManipulation.h
@@ -26,7 +26,7 @@
  * File Name: LDreferenceManipulation.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3e7f 27-January-2015
+ * Project Version: 3f1a 10-May-2015
  *
  *******************************************************************************/
 
