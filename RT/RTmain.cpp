e05cc73d96bee29b0192f6882ed99940bdd44f4d
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:32:43 +1100

    NO CHANGE RECORD

--- RT/RTmain.cpp
+++ RT/RTmain.cpp
@@ -25,7 +25,7 @@
  * File Name: RTmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Raytracer Functions
- * Project Version: 3d2d 14-April-2014
+ * Project Version: 3d2e 14-April-2014
  *
  *******************************************************************************/
 
@@ -235,7 +235,7 @@ int main(int argc,char **argv)
 
 		if (argumentExists(argc,argv,"-version"))
 		{
-			cout << "OpenRT.exe - Project Version: 3d2d 14-April-2014" << endl;
+			cout << "OpenRT.exe - Project Version: 3d2e 14-April-2014" << endl;
 			exit(1);
 		}
 	}
--- RT/RTmain.h
+++ RT/RTmain.h
@@ -25,7 +25,7 @@
  * File Name: RTmain.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Raytracer Functions
- * Project Version: 3d2d 14-April-2014
+ * Project Version: 3d2e 14-April-2014
  *
  *******************************************************************************/
 
