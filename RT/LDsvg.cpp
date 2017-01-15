dbdc4e8aa23339a9e72f515da5a55ef5bfaf62fe
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:38:09 +1100

    NO CHANGE RECORD

--- RT/LDsvg.cpp
+++ RT/LDsvg.cpp
@@ -26,7 +26,7 @@
  * File Name: LDsvg.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3h1d 14-November-2015
+ * Project Version: 3h1e 14-November-2015
  *
  *******************************************************************************/
 
