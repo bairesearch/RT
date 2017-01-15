66fd7edb6bb0933e8748c8ce3f3a6b371f91d5f7
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:34:12 +1100

    NO CHANGE RECORD

--- RT/RTmain.cpp
+++ RT/RTmain.cpp
@@ -26,7 +26,7 @@
  * File Name: RTmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Raytracer Functions
- * Project Version: 3e3b 01-September-2014
+ * Project Version: 3e4a 02-September-2014
  *
  *******************************************************************************/
 
@@ -232,7 +232,7 @@ int main(int argc,char **argv)
 
 		if (argumentExists(argc,argv,"-version"))
 		{
-			cout << "OpenRT.exe - Project Version: 3e3b 01-September-2014" << endl;
+			cout << "OpenRT.exe - Project Version: 3e4a 02-September-2014" << endl;
 			exit(1);
 		}
 	}
--- RT/XMLparserClass.h
+++ RT/XMLparserClass.h
@@ -26,7 +26,7 @@
  * File Name: XMLparserClass.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: XML Functions
- * Project Version: 3e3b 01-September-2014
+ * Project Version: 3e4a 02-September-2014
  *
  *******************************************************************************/
 
