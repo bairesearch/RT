a975cbb334236e6cae97b3cb41de7d23e29ebd6f
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:37:25 +1100

    NO CHANGE RECORD

--- RT/XMLrulesClass.h
+++ RT/XMLrulesClass.h
@@ -26,7 +26,7 @@
  * File Name: XMLrulesClass.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: XML Functions
- * Project Version: 3f5a 21-July-2015
+ * Project Version: 3f5b 21-July-2015
  *
  *******************************************************************************/
 
