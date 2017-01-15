983ddc9d7358fd8412f50eddcc7ca36cd7fe2821
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:37:58 +1100

    NO CHANGE RECORD

--- RT/LDmysql.cpp
+++ RT/LDmysql.cpp
@@ -26,7 +26,7 @@
  * File Name: LDmysql.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3f7c 16-August-2015
+ * Project Version: 3f7d 16-August-2015
  *
  * to test sql connection;
  * 1. uncomment int main()
