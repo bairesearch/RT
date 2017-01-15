d784b41cb055c9247109c60068ffb9ff753cc403
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:32:37 +1100

    NO CHANGE RECORD

--- RT/LDreferenceClass.cpp
+++ RT/LDreferenceClass.cpp
@@ -23,7 +23,7 @@
  * File Name: LDreferenceClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Generic Ldraw Construct Functions
- * Project Version: 3d2a 14-April-2014
+ * Project Version: 3d2b 14-April-2014
  *
  *******************************************************************************/
 
--- RT/SHAREDvector.h
+++ RT/SHAREDvector.h
@@ -23,7 +23,7 @@
  * File Name: SHAREDvector.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3d2a 14-April-2014
+ * Project Version: 3d2b 14-April-2014
  *
  *******************************************************************************/
 
