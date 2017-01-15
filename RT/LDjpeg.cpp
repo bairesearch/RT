5be36ca2b9760d46abc54e99516d11299e71819c
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:38:19 +1100

    NO CHANGE RECORD

--- RT/LDjpeg.cpp
+++ RT/LDjpeg.cpp
@@ -26,7 +26,7 @@
  * File Name: LDjpeg.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3h9a 09-December-2015
+ * Project Version: 3h9b 09-December-2015
  *
  *******************************************************************************/
 
