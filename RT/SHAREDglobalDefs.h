a5678a151ee82b5e7b80192e9ee857210c433a69
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:31:23 +1100

    NO CHANGE RECORD

--- RT/SHAREDglobalDefs.h
+++ RT/SHAREDglobalDefs.h
@@ -23,7 +23,7 @@
  * File Name: SHAREDglobalDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2012 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3c5c 21-December-2012
+ * Project Version: 3c6a 15-July-2013
  * Requirements: requires text parsed by RelEx (available in .CFF format <relations>)
  * Description: Generic Construct Functions: shared global definitions (configure to compile different BAI projects)
  *
