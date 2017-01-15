de03ca3f247321549061d730318eeeef2b96694e
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:39:42 +1100

    NO CHANGE RECORD

--- RT/SHAREDglobalDefs.h
+++ RT/SHAREDglobalDefs.h
@@ -41,13 +41,13 @@
 
 	//current/active tests;
 //#define COMPILE_CF
-//#define COMPILE_NLC
+#define COMPILE_NLC
 //#define COMPILE_GIA		//compileGIA.bat -> OpenGIA.exe
 //#define COMPILE_GIA_WITH_CE
 //#define COMPILE_OR		//compileOR.bat -> OpenOR.exe
 //#define COMPILE_CS		//compileCS.bat -> OpenCS.exe
 //#define COMPILE_RT		//compileRT.bat -> OpenRT.exe
-#define COMPILE_ANN_WITH_RT	//compileANNwithRT.bat -> OpenANN.exe
+//#define COMPILE_ANN_WITH_RT	//compileANNwithRT.bat -> OpenANN.exe
 //#define COMPILE_ANN		//compileANN.bat -> OpenANN.exe
 //#define COMPILE_LRRC		//compileLRRC.bat -> OpenLRRC.exe
 //#define COMPILE_OLC		//compileOLC.bat -> OLC.exe
@@ -70,7 +70,7 @@
 	OS DEPENDENT VARIABLES;
 	***********/
 
-//#define LINUX
+#define LINUX
 #ifndef LINUX
 	//else assume windows (affects default parts directory only - this is ISO C++ so it should compile on any platform)
 	//#include <windows.h>	//incompatible with LDjpeg.h (must only include windows.h when need to)
