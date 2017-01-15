e05e6a62fa57784f0bbd0808910f5ac338c2ad9d
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:34:04 +1100

    NO CHANGE RECORD

--- RT/SHAREDglobalDefs.h
+++ RT/SHAREDglobalDefs.h
@@ -52,11 +52,11 @@ using namespace std;
 
 	//current/active tests;
 //#define COMPILE_CF
-#define COMPILE_NLC
+//#define COMPILE_NLC
 //#define COMPILE_GIA		//compileGIA.bat -> OpenGIA.exe
 //#define COMPILE_GIA_WITH_CE
 //#define COMPILE_OR		//compileOR.bat -> OpenOR.exe
-//#define COMPILE_CS		//compileCS.bat -> OpenCS.exe
+#define COMPILE_CS		//compileCS.bat -> OpenCS.exe
 //#define COMPILE_RT		//compileRT.bat -> OpenRT.exe
 //#define COMPILE_ANN_WITH_RT	//compileANNwithRT.bat -> OpenANN.exe
 //#define COMPILE_ANN		//compileANN.bat -> OpenANN.exe
