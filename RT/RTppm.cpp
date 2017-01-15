4cd794111b0ef762ec021d5cd9e160978c6eecb1
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:39:39 +1100

    NO CHANGE RECORD

--- RT/RTppm.cpp
+++ RT/RTppm.cpp
@@ -26,7 +26,7 @@
  * File Name: RTppm.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: Raytracer Functions
- * Project Version: 3i16a 27-August-2016
+ * Project Version: 3i17a 20-September-2016
  *
  *******************************************************************************/
 
--- RT/SHAREDvector.h
+++ RT/SHAREDvector.h
@@ -26,7 +26,7 @@
  * File Name: SHAREDvector.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3i16a 27-August-2016
+ * Project Version: 3i17a 20-September-2016
  *
  *******************************************************************************/
 
