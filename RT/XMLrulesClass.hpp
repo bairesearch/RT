640a2f01a175a66302c94e7186f5ca7af88dc330
Richard Baxter <richard.baxter@baxterai.com>
2017-03-06 11:30:31 +1100

    RT 3j3e - update license headers

--- RT/XMLrulesClass.hpp
+++ RT/XMLrulesClass.hpp
@@ -2,9 +2,8 @@
  *
  * This file is part of BAIPROJECT.
  *
- * BAIPROJECT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License version 3
- * only, as published by the Free Software Foundation. The use of
+ * BAIPROJECT is licensed under the GNU Affero General Public License
+ * version 3, as published by the Free Software Foundation. The use of
  * intermediary programs or interfaces including file i/o is considered
  * remote network interaction. This does not imply such arrangements
  * do not constitute derivative works.
@@ -26,7 +25,7 @@
  * File Name: XMLrulesClass.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2017 Baxter AI (baxterai.com)
  * Project: XML Functions
- * Project Version: 3j3b 26-January-2017
+ * Project Version: 3j3e 26-January-2017
  *
  *******************************************************************************/
 
