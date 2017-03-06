66d397870484d1c7d006bf09778907fc2562fa45
Richard Baxter <richard.baxter@baxterai.com>
2017-03-06 11:03:47 +1100

    RT 3j3d - update license headers

--- RT/SHAREDvars.cpp
+++ RT/SHAREDvars.cpp
@@ -2,9 +2,8 @@
  *
  * This file is part of BAIPROJECT.
  *
- * BAIPROJECT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License version 3
- * only, as published by the Free Software Foundation. The use of
+ * BAIPROJECT is licensed under the GNU Affero General Public License
+ * version 3, as published by the Free Software Foundation. The use of
  * intermediary programs or interfaces including file i/o is considered
  * remote network interaction. This does not imply such arrangements
  * do not constitute derivative works.
