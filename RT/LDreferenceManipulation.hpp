bfd34757b7999bc143274d4fd9b134a6263e2f31
Richard Baxter <richard.baxter@baxterai.com>
2019-09-10 06:06:19 +1000

    NO CHANGE RECORD

--- RT/LDreferenceManipulation.hpp
+++ RT/LDreferenceManipulation.hpp
@@ -26,7 +26,7 @@
  * File Name: LDreferenceManipulation.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2019 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3n1b 10-August-2019
+ * Project Version: 3n1c 10-August-2019
  * /
  *******************************************************************************/
 
--- RT/RTparser.hpp
+++ RT/RTparser.hpp
@@ -26,7 +26,7 @@
  * File Name: RTparser.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2019 Baxter AI (baxterai.com)
  * Project: Raytracer Functions
- * Project Version: 3n1b 10-August-2019
+ * Project Version: 3n1c 10-August-2019
  * Description: A simple parser for TAL files
  * /
  *******************************************************************************/
