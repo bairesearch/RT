4dc59de908eb697b3378351b06dad58a28068f7e
Richard Baxter <richard.baxter@baxterai.com>
2020-08-06 02:07:25 +1000

    NO CHANGE RECORD

--- RT/LDopengl.cpp
+++ RT/LDopengl.cpp
@@ -26,7 +26,7 @@
  * File Name: LDopengl.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3n2a 10-March-2020
+ * Project Version: 3n3a 28-May-2020
  * /
  *******************************************************************************/
 
