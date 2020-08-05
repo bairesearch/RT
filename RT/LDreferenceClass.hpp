cd783a3c7ec418df336214cc8f2abdb3a8ecb90a
Richard Baxter <richard.baxter@baxterai.com>
2020-08-06 02:07:26 +1000

    NO CHANGE RECORD

--- RT/LDreferenceClass.hpp
+++ RT/LDreferenceClass.hpp
@@ -26,7 +26,7 @@
  * File Name: LDreferenceClass.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Generic Ldraw Construct Functions
- * Project Version: 3n3a 28-May-2020
+ * Project Version: 3n3b 28-May-2020
  * /
  *******************************************************************************/
 
