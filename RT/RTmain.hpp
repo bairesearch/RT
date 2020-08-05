988bd72df904556aa7732e4171b1023fe2aa376b
Richard Baxter <richard.baxter@baxterai.com>
2020-08-06 02:07:28 +1000

    NO CHANGE RECORD

--- RT/RTmain.hpp
+++ RT/RTmain.hpp
@@ -26,7 +26,7 @@
  * File Name: RTmain.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Raytracer Functions
- * Project Version: 3n3c 28-May-2020
+ * Project Version: 3n4a 02-June-2020
  * /
  *******************************************************************************/
 
