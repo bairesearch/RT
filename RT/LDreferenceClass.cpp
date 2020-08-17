e84f580d7fc8b8ba8809cf55ac0a338b21a39e2a
Richard Baxter <richard.baxter@baxterai.com>
2020-08-17 19:06:01 +1000

    NO CHANGE RECORD

--- RT/LDreferenceClass.cpp
+++ RT/LDreferenceClass.cpp
@@ -26,7 +26,7 @@
  * File Name: LDreferenceClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Generic Ldraw Construct Functions
- * Project Version: 3n7c 17-August-2020
+ * Project Version: 3n7d 17-August-2020
  * /
  *******************************************************************************/
 
